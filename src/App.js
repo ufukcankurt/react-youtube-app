01c3ad515b4456da8a1816a993830064b72ebbd0
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-05 07:05:55 +0300

    feat(App.js): create page layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+  
+`
+
+const Main = styled.div`
+flex: 7;`
+const Wrapper = styled.div``
+
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          video cards
+        </Wrapper>
+      </Main>
+    </Container>
   );
 }
 

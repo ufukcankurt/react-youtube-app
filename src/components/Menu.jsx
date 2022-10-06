b843672d4c90ef47eff04699894bfd4d61284f08
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-06 13:22:37 +0300

    refactor(menu): update theme background-color

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,7 +20,7 @@ import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightne
 
 const Container = styled.div`
   flex: 1;
-  background-color: ${({ theme }) => theme.bg};
+  background-color: ${({ theme }) => theme.bgLighter};
   height: 100vh;
   color: ${({ theme }) => theme.text};
   font-size: 14px;

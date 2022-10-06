daafb2ba3295e0309d30db5881853d5dd37eb83e
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-06 13:34:58 +0300

    feat(pages): create pages with react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+
 const Container = styled.div`
   display: flex;
   
@@ -22,13 +30,22 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Main>
-          <Navbar />
-          <Wrapper>
-            video cards
-          </Wrapper>
-        </Main>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    
+`
+
+const Home = () => {
+  return (
+    <Container>
+      Home
+    </Container>
+  )
+}
+
+export default Home

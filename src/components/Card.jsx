0b449a74bf2bdfd3f10d4a24e1ff1fff6ef5ff22
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-10 15:07:01 +0300

    feat(card): styled components using props with conditions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,24 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    width: 360px;
-    margin-bottom: 45px;
+    width:  ${(props) => props.type !== 'sm' && '360px'};
+    margin-bottom: ${(props) => props.type === 'sm' ? '10px' : "45px"};
     cursor: pointer;
+    display: ${(props) => props.type === 'sm' && 'flex'};
+    gap: 10px;
 `
 
 const Image = styled.img`
     width: 100%;
-    height: 202px;
+    height: ${(props) => props.type === 'sm' ? '120px' : "202px"};
     background-color: #999;
+    flex: 1;
 `
 const Details = styled.div`
     display: flex;
-    margin-top: 16px;
+    margin-top:  ${(props) => props.type !== 'sm' && '16px'};
     gap: 12px;
+    flex: 1;
 `
 
 const ChannelImage = styled.img`
@@ -24,6 +28,7 @@ const ChannelImage = styled.img`
     height: 36px;
     border-radius: 50%;
     background-color: #999;
+    display: ${(props) => props.type === 'sm' && 'none'};
 `
 
 const Texts = styled.div`
@@ -47,13 +52,13 @@ font-size: 14px;
 color: ${({ theme }) => theme.textSoft};
 `
 
-const Card = () => {
+const Card = ({ type }) => {
     return (
-        <Link to="/video/test" style={{textDecoration:"none"}}>
-            <Container>
-                <Image src='https://i.ytimg.com/vi/jsZoR1kkq6s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB7UWLw6X72QOJqd53EnIZE8jAyqQ' />
-                <Details>
-                    <ChannelImage src='https://yt3.ggpht.com/ytc/AMLnZu94pzTFiSDqYIvXn40JdctQCOxK2fnAMEy0zdL6kA=s68-c-k-c0x00ffffff-no-rj' />
+        <Link to="/video/test" style={{ textDecoration: "none" }}>
+            <Container type={type}>
+                <Image type={type} src='https://i.ytimg.com/vi/jsZoR1kkq6s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB7UWLw6X72QOJqd53EnIZE8jAyqQ' />
+                <Details type={type}>
+                    <ChannelImage type={type} src='https://yt3.ggpht.com/ytc/AMLnZu94pzTFiSDqYIvXn40JdctQCOxK2fnAMEy0zdL6kA=s68-c-k-c0x00ffffff-no-rj' />
                     <Texts>
                         <Title>How to make a YouTube clone with ReactJS</Title>
                         <ChannelName>Codevolution</ChannelName>

714699cdab4f21b9e9820b5e5760218a10fef4c1
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-12 10:47:59 +0300

    feat(App.js): add a new signin route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
+import Signin from "./pages/Signin";
 
 const Container = styled.div`
   display: flex;
@@ -40,6 +41,7 @@ function App() {
               <Routes>
                 <Route path="/">
                   <Route index element={<Home />} />
+                  <Route path="signin" element={<Signin />} />
                   <Route path="video">
                     <Route path=":id" element={<Video />} />
                   </Route>

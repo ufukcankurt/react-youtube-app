740d8eebc500efda4ed31fbb931828d26a1214bf
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-12 10:47:18 +0300

    feat(sigin): create login page component

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 56px);
+    color: ${({ theme }) => theme.text};
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-color: ${({ theme }) => theme.bgLighter};
+    border: 1px solid ${({ theme }) => theme.soft};
+    padding: 20px 50px;
+    gap: 10px;
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+`
+
+const SubTitle = styled.h2`
+    font-size: 20px;
+    font-weight: 300;
+`
+
+const Input = styled.input`
+    border: 1px solid ${({ theme }) => theme.soft};
+    border-radius: 3px;
+    padding: 10px;
+    background-color: transparent;
+    width: 100%;
+`
+
+const Button = styled.button`
+    border-radius: 3px;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-weight: 500;
+    background-color: ${({ theme }) => theme.soft};
+    color: ${({ theme }) => theme.textSoft};
+`
+
+const More = styled.div`
+    display: flex;
+    margin-top: 10px;
+    font-size: 12px;
+    color: ${({ theme }) => theme.textSoft};
+`
+
+const Links = styled.div`
+    margin-left: 50px;
+`
+
+const Link = styled.span`
+    margin-left: 30px;
+`
+
+const Signin = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>Sign In</Title>
+                <SubTitle>Sign in to your account</SubTitle>
+                <Input placeholder="username" />
+                <Input type="password" placeholder="password" />
+                <Button>Sign in</Button>
+                <Title>Or</Title>
+                <Input placeholder="username" />
+                <Input placeholder="email" />
+                <Input type="password" placeholder="password" />
+                <Button>Sign up</Button>
+            </Wrapper>
+            <More>
+                English(USA)
+                <Links>
+                    <Link>Help</Link>
+                    <Link>Privacy</Link>
+                    <Link>Terms</Link>
+                </Links>
+            </More>
+        </Container>
+    )
+}
+
+export default Signin

2bada3e514473d4ef0d836a936325f4f01a12fd2
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-01 16:50:56 +0300

    feat: add a condition to login button to show button or not

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -20,6 +20,9 @@ import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 
+import { useSelector } from "react-redux";
+
+
 const Container = styled.div`
   flex: 1;
   background-color: ${({ theme }) => theme.bgLighter};
@@ -86,6 +89,7 @@ const Title = styled.h2`
 `
 
 const Menu = ({ darkMode, setDarkMode }) => {
+  const { currentUser } = useSelector(state => state.user)
   return (
     <Container>
       <Wrapper>
@@ -121,13 +125,13 @@ const Menu = ({ darkMode, setDarkMode }) => {
           History
         </Item>
         <Hr />
-        <Login>
+        {!currentUser && <> <Login>
           Sign in to like videos, comment, and subscribe.
           <Link to="/signin" style={{ textDecoration: "none" }}>
             <Button> <AccountCircleOutlinedIcon /> SIGN IN</Button>
           </Link>
         </Login>
-        <Hr />
+          <Hr /></>}
         <Title>BEST OF YOUTUBE</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import styled from "styled-components"
 
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
 import { Link } from 'react-router-dom';
+
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
 position: sticky;
@@ -52,7 +55,25 @@ const Button = styled.button`
   gap: 5px;
 `
 
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text};
+`
+
+const Avatar = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #999;
+`
+
 const Navbar = () => {
+
+  const { currentUser } = useSelector(state => state.user)
+
   return (
     <Container>
       <Wrapper>
@@ -60,9 +81,13 @@ const Navbar = () => {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to="/signin" style={{ textDecoration: "none" }}>
+        {currentUser ? <User>
+          <VideoCallOutlinedIcon />
+          <Avatar />
+          {currentUser.name}
+        </User> : <Link to="/signin" style={{ textDecoration: "none" }}>
           <Button> <AccountCircleOutlinedIcon /> SIGN IN</Button>
-        </Link>
+        </Link>}
       </Wrapper>
     </Container>
   )

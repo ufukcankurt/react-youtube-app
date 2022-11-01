618fa4b5d8d8485aa2bf5e97e90e0da5348b3290
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-01 16:53:34 +0300

    feat(sigin): create dispatch to get currentUser to global store

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import axios from "axios"
+import {useDispatch} from 'react-redux'
+import { loginFailure, loginStart, loginSuccess } from '../redux/userSlice'
 
 const Container = styled.div`
     display: flex;
@@ -69,17 +71,19 @@ const Signin = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
 
     const handleLogin = async (e) => {
         e.preventDefault();
+        dispatch(loginStart())
         try {
             const res = await axios.post(`${FETCH}auth/signin`, {
                 name,
                 password,
             });
-            console.log(res.data);
+            dispatch(loginSuccess(res.data))
         } catch (err) {
-            console.log(err);
+            dispatch(loginFailure())
         }
     }
 

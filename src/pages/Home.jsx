8ee5f8ce74b7cb5b7b3501ea476a1914ce54c8fc
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-01 14:31:32 +0300

    feat(home): fetch videos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Card from '../components/Card'
+import axios from "axios"
 
 const Container = styled.div`
     display: flex;
@@ -8,36 +9,23 @@ const Container = styled.div`
     flex-wrap: wrap;
 `
 
-const Home = () => {
+const Home = ({ type }) => {
+
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            const res = await axios.get(`/videos/${type}`);
+            setVideos(res.data);
+        }
+        fetchVideos();
+    }, [type])
+
     return (
         <Container>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {videos.map((video) =>
+                <Card key={video._id} video={video} />
+            )}
         </Container>
     )
 }

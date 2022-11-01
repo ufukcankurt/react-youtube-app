067ec367a0111f1dbb4ec72c98fc3954308a525b
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-01 16:52:10 +0300

    feat(index.js): add provider & persistGate for redux store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store,persistor } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );

3fc97ffa964274ebd4d584721e26188ec4007c7d
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-02 22:49:44 +0300

    refactore(redux): rearrange videoslice's state and reducer

--- src/redux/videoSlice.js
+++ src/redux/videoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    currentUser: null,
+    currentVideo: null,
     loading: false,
     error: false,
 }
@@ -10,23 +10,20 @@ export const videoSlice = createSlice({
     name: 'video',
     initialState,
     reducers: {
-        loginStart: (state) => {
+        fetchStart: (state) => {
             state.loading = true;
         },
-        loginSuccess: (state, action) => {
+        fetchSuccess: (state, action) => {
             state.loading = false;
-            state.currentUser = action.payload;
+            state.currentVideo = action.payload;
         },
-        loginFailure: (state) => {
+        fetchFailure: (state) => {
             state.loading = false;
             state.error = true;
-        },
-        logout: (state) => {
-            return initialState;
         }
     },
 })
 
-export const { loginStart, loginSuccess, loginFailure, logout } = videoSlice.actions;
+export const { fetchStart, fetchSuccess, fetchFailure } = videoSlice.actions;
 
 export default videoSlice.reducer;

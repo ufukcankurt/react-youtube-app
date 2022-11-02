55cf088c185b82ed816516ce428e591bac33ed4b
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-02 22:50:58 +0300

    refactor(card): make video link as a dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -66,7 +66,7 @@ const Card = ({ type, video }) => {
         fetchChannel();
     }, [video.userId])
     return (
-        <Link to="/video/test" style={{ textDecoration: "none" }}>
+        <Link to={`/video/${video._id}`} style={{ textDecoration: "none" }}>
             <Container type={type}>
                 <Image type={type} src={video.img} />
                 <Details type={type}>

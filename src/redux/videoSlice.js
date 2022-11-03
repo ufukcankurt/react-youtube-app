80a5b3fb92ab4481471c215f2fbeb08cffd6816b
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-03 09:49:02 +0300

    feat(redux): create like & dislike actions at videoSlice

--- src/redux/videoSlice.js
+++ src/redux/videoSlice.js
@@ -20,10 +20,22 @@ export const videoSlice = createSlice({
         fetchFailure: (state) => {
             state.loading = false;
             state.error = true;
+        },
+        like: (state, action) => {
+            if (!state.currentVideo.likes.includes(action.payload)) {
+                state.currentVideo.likes.push(action.payload);
+                state.currentVideo.dislikes.splice(state.currentVideo.dislikes.findIndex((userId) => userId === action.payload), 1);
+            }
+        },
+        dislike: (state, action) => {
+            if (!state.currentVideo.dislikes.includes(action.payload)) {
+                state.currentVideo.dislikes.push(action.payload);
+                state.currentVideo.likes.splice(state.currentVideo.likes.findIndex((userId) => userId === action.payload), 1);
+            }
         }
     },
 })
 
-export const { fetchStart, fetchSuccess, fetchFailure } = videoSlice.actions;
+export const { fetchStart, fetchSuccess, fetchFailure, like, dislike } = videoSlice.actions;
 
 export default videoSlice.reducer;

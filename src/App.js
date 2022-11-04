0b59bf6aeafe1ba44910e5c4d9016fad4695a0ca
ufukcankurt <ufuk.cnk@gmail.com>
2022-11-04 16:52:18 +0300

    feat(App.js): create a new route that is search

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import Signin from "./pages/Signin";
+import Search from "./pages/Search";
 
 const Container = styled.div`
   display: flex;
@@ -43,6 +44,7 @@ function App() {
                   <Route index element={<Home type="random" />} />
                   <Route path="trends" element={<Home type="trend" />} />
                   <Route path="subscriptions" element={<Home type="sub" />} />
+                  <Route path="search" element={<Search />} />
                   <Route path="signin" element={<Signin />} />
                   <Route path="video">
                     <Route path=":id" element={<Video />} />
